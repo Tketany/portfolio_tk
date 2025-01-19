8fa73a9e3252358550131b20961bf2d3928466ad
Tketany <tkettany@gmail.com>
2025-01-19 20:47:37 +0200

    Finish Hero Section, Add TextGenerator effect and magic button

--- my-portfolio/components/Hero.tsx
+++ my-portfolio/components/Hero.tsx
@@ -1,4 +1,7 @@
+import { FaLocationArrow } from 'react-icons/fa'
+import MagicButton from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
@@ -9,7 +12,7 @@ const Hero = () => {
             <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill="blue" />
         </div>
         <div>
-          <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.05] flex items-center justify-center absolute top-0 left-0 ">
+          <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.2] relative flex top-0 left-0 items-center justify-center ">
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
             
             
@@ -18,6 +21,25 @@ const Hero = () => {
                 <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 '>
                   Dynamic Web Magic with Next.js
                 </h2>
+                <TextGenerateEffect 
+                
+                className='text-center text-[40px] md:text-4xl lg:text-6xl'
+                words='Transforming Concepts Into Seamless User Experiences '
+                />
+
+                <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+                  Hi, I&apos;m TKET! A Mechatronics Engineer based in Lebanon
+
+                </p>
+
+                <a href="#about">
+                  <MagicButton 
+                    title="Show My Work"
+                    icon={<FaLocationArrow />}
+                    position='right'
+        
+                  />
+                </a>
 
               </div>
 
